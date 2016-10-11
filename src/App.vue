41677747dce1e9964f44a81cd1d5cbe50464b2ff
iamthtm <p_1624_n@hotmail.com>
2016-10-11 14:57:18 +0700

    test

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<cal></cal>
+</template>
+
+<script>
+import cal from 'components/Hello'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {},
+  components: {
+    cal
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <!-- <img class="logo" src="./assets/logo.png">
+    <ttt></ttt> -->
+    <button @click="add(1)">1</button>
+    <button @click="add(2)">2</button>
+    <button @click="add(3)">3</button>
+    <button @click="add(4)">4</button>
+    <button @click="add(5)">5</button>
+    <button @click="add(6)">6</button>
+    <button @click="add(7)">7</button>
+    <button @click="add(8)">8</button>
+    <button @click="add(9)">9</button>
+    <button @click="add(0)">0</button>
+    <button @click="buk()">+</button>
+    <button @click="del()">-</button>
+    <button @click="total()">=</button>
+    <button @click="clear()">C</button>
+    <br> {{ num1 }} {{ operation }} {{ num2 }} = {{ sum }}
+  </div>
+</template>
+
+<script>
+// import ttt from './components/Hello' // เรียก hello
+
+export default {
+  components: {
+    // ttt
+  },
+  methods: {
+    add: function (num) {
+      if (this.pass === false) {
+        this.num1 += num
+        console.log('num1', this.num1)
+      } else if (this.pass === true) {
+        this.num2 += num
+        console.log('num2', this.num2)
+      }
+    },
+    buk: function () {
+      this.pass = true
+      this.operation = '+'
+    },
+    del: function () {
+      this.pass = true
+      this.operation = '-'
+    },
+    total: function () {
+      // this.sum = this.num1 + this.num2
+      if (this.operation === '+') {
+        this.sum = parseInt(this.num1) + parseInt(this.num2)
+      } else if (this.operation === '-') {
+        this.sum = parseInt(this.num1) - parseInt(this.num2)
+      }
+    },
+    clear: function () {
+      this.num1 = 0
+      this.num2 = 0
+      this.sum = 0
+      this.pass = false
+      this.operation = ''
+    }
+  },
+  data () {
+    return {
+      sum: 0,
+      num1: '',
+      num2: '',
+      pass: false,
+      operation: ''
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
